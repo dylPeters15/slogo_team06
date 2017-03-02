9c25e52fec0ea55f5637736fbfc0bf44c123f46b
Yilin Gao <yilin.gao@duke.edu>
2017-03-01 20:28:00 -0500

    Add if commands

--- src/backend/interpreter/Interpreter.java
+++ src/backend/interpreter/Interpreter.java
@@ -21,64 +21,57 @@ public class Interpreter {
 	private ProgramParser type;
 	ResourceBundle resources;
 	private ObservableMap<String,String> variables;
-	
+
 	public Interpreter(StatesList<State> statesList, ObservableMap<String, String> variables){
 		this.variables = variables;
 		this.statesList = statesList;
-		 langParser = new ProgramParser();
-		 type = new ProgramParser();
-		 setLanguage(DEF_LANG);
-		 
+		langParser = new ProgramParser();
+		type = new ProgramParser();
+		setLanguage(DEF_LANG);
 	}		
+	
 	private void setParserPatterns(String lang) {
-		
+
 		langParser.clearPatterns();
 		type.clearPatterns();
-		
+
 		// these are more specific, so add them first to ensure they are checked first
-		 langParser.addPatterns(lang);
-	    // these are more general so add them at the end
-	     langParser.addPatterns(SYNTAX);
-	     type.addPatterns(SYNTAX);
+		langParser.addPatterns(lang);
+		// these are more general so add them at the end
+		langParser.addPatterns(SYNTAX);
+		type.addPatterns(SYNTAX);
 	}
-	
+
 	public void interpret(String text) throws SlogoException{
 		try{
-		parseText(text.split(WHITESPACE));
+			parseText(text.split(WHITESPACE));
 		}
 		catch (SlogoException e){
 			translateError(e);
 			throw e;
 		}
 	}
-	
+
 	private void parseText (String[] text) throws SlogoException {	
 		LinkedList<String> words = new LinkedList<String>(Arrays.asList(text));
 		recursiveParse(words);
-       
-    }
-	
+	}
+
 	private double recursiveParse(LinkedList<String> words) throws SlogoException{
-				
 		if(words.isEmpty()){
-				throw new SlogoException("IncorrectNumOfParameters");
-			}
+			throw new SlogoException("IncorrectNumOfParameters");
+		}
 		
 		String word = words.pop();
-		
+
 		if(isConstant(word)){
 			return Double.parseDouble(word);
 		}
-
 		else if(type.getSymbol(word).equals("Command")){
-			
-			
 			Command com = null;
-			
-				System.out.println(langParser.getSymbol(word));
-				com = Command.getCommand(langParser.getSymbol(word), statesList);	
+			System.out.println(langParser.getSymbol(word));
+			com = Command.getCommand(langParser.getSymbol(word), statesList);	
 			try{
-				
 				if(com.needsVarParams()){
 					com.setVarMap(variables);
 					List<String> params = new ArrayList<String>();
@@ -88,13 +81,34 @@ public class Interpreter {
 							params.add(word);
 						}
 						else if(com.paramsNeeded().get(i).equals("Constant")){
-								words.addFirst(word);
-								params.add(Double.toString(recursiveParse(words)));	
+							words.addFirst(word);
+							params.add(Double.toString(recursiveParse(words)));	
 						}
 					}
-					
 					return com.runCommand(params);
-		
+				}
+				else if (com.needsPriorCheck()) { // if and if-else
+					double condition = recursiveParse(words);
+					if (condition != 0) {
+						if(com.numParamsNeeded() == 1){
+							return com.runCommand(condition);
+						}
+						if(com.numParamsNeeded() == 2){
+							return com.runCommand(condition,recursiveParse(words));
+						}
+						else {
+							return com.runCommand();
+						}
+					}
+					else {
+						if(com.numParamsNeeded() == 2){
+							word = words.pop();
+							while (!type.getSymbol(word).equals("ListEnd")) {
+								word = words.pop();
+							}
+						}
+						return 0;
+					}
 				}
 				else{
 					if(com.numParamsNeeded() == 1){
@@ -112,7 +126,7 @@ public class Interpreter {
 				e.setText(e.getText()+"->"+word);
 			}
 		}
-		if(type.getSymbol(word).equals("Variable")){
+		else if(type.getSymbol(word).equals("Variable")){
 			try{
 				return Double.parseDouble(variables.get(word.substring(1)));
 			}
@@ -120,16 +134,24 @@ public class Interpreter {
 				throw new SlogoException("IncorrectParamType");
 			}
 		}
+		else if (type.getSymbol(word).equals("ListStart")) {
+			return recursiveParse(words);
+		}
+		else if (type.getSymbol(word).equals("ListEnd")) {
+			
+		}
 		else{
 			System.out.println("Not a variable?");
 		}
-		
+
 		throw new SlogoException("IncorrectNumOfParameters");
-		
+
 	}
+	
 	private boolean isConstant(String word) {
 		return langParser.getSymbol(word).equals("Constant");
 	}
+	
 	public void translateError(SlogoException e){
 		int indexOfCustomMessage = e.getText().indexOf(';');
 		if(indexOfCustomMessage==-1){
@@ -138,13 +160,10 @@ public class Interpreter {
 		String toReplace = e.getText().substring(0,indexOfCustomMessage);
 		e.setText(e.getText().replaceAll(toReplace, resources.getString(toReplace)));
 	}
-	
+
 	public void setLanguage(String lang) {
 		resources = ResourceBundle.getBundle(DEF_LANG);
 		setParserPatterns(lang);
 	}
-	
-	
-	
-	
+
 }
