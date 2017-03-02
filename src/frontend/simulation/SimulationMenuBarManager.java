b6b19293b5874a4b8e7753f55f501e42a8ebcd0c
Dylan Peters <dlp22@duke.edu>
2017-03-01 23:48:16 -0500

    Added languages to variable table prompt

--- src/frontend/simulation/SimulationMenuBarManager.java
+++ src/frontend/simulation/SimulationMenuBarManager.java
@@ -118,10 +118,12 @@ class SimulationMenuBarManager extends SimulationPaneManagerChild<SimulationMenu
 		choose.setInitialDirectory(new File(System.getProperty("user.dir")));
 		choose.getExtensionFilters().setAll(new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
 		File file = choose.showOpenDialog(null);
-        Image image = new Image(file.toURI().toString());
-		if (getDelegate() != null){
-			getDelegate().setTurtleImage(image);
-		}
+        if (file != null){
+        	Image image = new Image(file.toURI().toString());
+        	if (getDelegate() != null && image != null){
+    			getDelegate().setTurtleImage(image);
+    		}
+        }
 	}
 
 	private void setBackgroundColor(Color color) {
