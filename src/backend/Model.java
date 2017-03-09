768666866eb9d505a9bc3e0f82cb6ec2909f2ed2
Tavo Loaiza <edgar.loaiza@duke.edu>
2017-03-08 22:47:56 -0500

    Added passCommands list to model for saving to a file

--- src/backend/Model.java
+++ src/backend/Model.java
@@ -1,5 +1,8 @@
 package backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Exceptions.SlogoException;
 import backend.interpreter.Interpreter;
 import backend.states.*;
@@ -13,6 +16,7 @@ import javafx.collections.ObservableMap;
 public class Model {
 	private StatesList<State> statesList;
 	private ObservableMap<String,String> variables;
+	private List<String> pastCommands;
 	
 	public StatesList<State> getStatesList() {
 		 return statesList;
@@ -25,10 +29,12 @@ public class Model {
 		statesList = new StatesList<State>();
 		statesList.add(new State());
 		interpreter = new Interpreter(statesList,variables);
+		pastCommands = new ArrayList<String>();
 	}
 	
 	public void interpret(String text) throws SlogoException{
 		interpreter.interpret(text);	
+		pastCommands.add(text);
 		System.out.println("All turtles: "+statesList.getLast().getActorMap().keySet());
 		System.out.println("Active turtles: "+statesList.getLast().getActiveList());
 		for(int id:statesList.getLast().getActiveList()){
@@ -43,6 +49,14 @@ public class Model {
 	
 	public ObservableMap<String,String> getVariables(){
 		return variables;
+	}
+	
+	public List<String> getPastCommands(){
+		return pastCommands;
+	}
+	
+	public void setPastCommands(List<String> list){
+		pastCommands = list;
 	}
 	
 }
--- src/backend/states/State.java
+++ src/backend/states/State.java
@@ -127,7 +127,6 @@ public class State {
 		this.clear = clear;
 	}
 
-
 	
 	
 }
